193adeb178f1168b6bbd61a59d02705f599e26ac
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-11-15 15:48:10 +0000

    Test separate function

--- weather.py
+++ weather.py
@@ -4,16 +4,17 @@ import random
 
 import datapoint
 
-class WeatherSkill():
+def getforecast(opsdroid, location):
+    api_key = opsdroid.config["skills"]["weather"]["api-key"]
+    conn = datapoint.connection(api_key=api_key)
+    site = conn.get_nearest_site(-0.124626, 51.500728)
+    return conn.get_forecast_for_site(site.id, "3hourly")
 
-    @match_regex(r'what is the weather in (.*)\?')
-    async def whatistheweather(opsdroid, message):
-        location = message.regex.group(1)
-        api_key = opsdroid.config["skills"]["weather"]["api-key"]
-        conn = datapoint.connection(api_key=api_key)
-        site = conn.get_nearest_site(-0.124626, 51.500728)
-        forecast = conn.get_forecast_for_site(site.id, "3hourly")
-        current_timestep = forecast.days[0].timesteps[0]
+@match_regex(r'what is the weather in (.*)\?')
+async def whatistheweather(opsdroid, message):
+    location = message.regex.group(1)
+    forecast = getforecast(opsdroid, location)
 
-        await message.respond("It looks like it is " +
-                              current_timestep.weather.text + " in " + location)
+    await message.respond("It looks like it is " +
+                          forecast.days[0].timesteps[0].weather.text +
+                          " in " + location)
