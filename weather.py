18d3878b9e2e3778ae19be2a3ce6ec369956eddc
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-11-15 11:02:29 +0000

    Get config

--- weather.py
+++ weather.py
@@ -7,5 +7,5 @@ import datapoint
 @match_regex(r'what is the weather in (.*)\?')
 async def whatistheweather(opsdroid, message):
     location = message.regex.group(1)
-    api_key = opsdroid.config.skills.weather.api-key
+    api_key = opsdroid.config["skills"]["weather"]["api-key"]
     await message.respond(location + " " + api_key)
