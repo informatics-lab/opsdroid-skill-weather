65b9c6dcb5bb15b4271e3ae77f1d24f2d78f10f6
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-02-01 13:12:54 +0000

    Update for v0.6.0

--- weather.py
+++ weather.py
@@ -4,8 +4,8 @@ import random
 
 import datapoint
 
-def getforecast(opsdroid, location):
-    api_key = opsdroid.config["skills"]["weather"]["api-key"]
+def getforecast(config, location):
+    api_key = config["api-key"]
     conn = datapoint.connection(api_key=api_key)
     site = conn.get_nearest_site(-0.124626, 51.500728)
     return conn.get_forecast_for_site(site.id, "3hourly")
@@ -13,7 +13,7 @@ def getforecast(opsdroid, location):
 @match_regex(r'what is the weather in (.*)\?')
 async def whatistheweather(opsdroid, config, message):
     location = message.regex.group(1)
-    forecast = getforecast(opsdroid, location)
+    forecast = getforecast(config, location)
 
     await message.respond("It looks like it is {} in {}".format(
                               forecast.days[0].timesteps[0].weather.text,
