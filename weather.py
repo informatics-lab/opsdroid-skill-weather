a4c4f4415d71507d9701828be2e93f5c06ff8994
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2017-02-01 13:24:42 +0000

    Add test weather skill

--- weather.py
+++ weather.py
@@ -1,6 +1,7 @@
-from opsdroid.matchers import match_regex
+from opsdroid.matchers import match_regex, match_apiai_action
 import logging
 import random
+import aiohttp
 
 import datapoint
 
@@ -10,6 +11,9 @@ def getforecast(config, location):
     site = conn.get_nearest_site(-0.124626, 51.500728)
     return conn.get_forecast_for_site(site.id, "3hourly")
 
+def weather_build_url(location):
+    return "https://shlmog4lwa.execute-api.eu-west-1.amazonaws.com/dev/datapoint?location=Milton%20Keynes"
+
 @match_regex(r'what is the weather in (.*)\?')
 async def whatistheweather(opsdroid, config, message):
     location = message.regex.group(1)
@@ -18,3 +22,12 @@ async def whatistheweather(opsdroid, config, message):
     await message.respond("It looks like it is {} in {}".format(
                               forecast.days[0].timesteps[0].weather.text,
                               location))
+
+@match_apiai_action("weather.location")
+async def weather_in_location(opsdroid, config, message):
+    location = message.apiai["result"]["parameters"]["geo-city"]
+    logging.debug(location)
+    async with aiohttp.ClientSession() as session:
+            async with session.get(weather_build_url(location)) as resp:
+                json = await resp.json()
+                await message.respond(json["properties"]["forecast"]["text"]["regional"])
